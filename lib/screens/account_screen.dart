c6562a591911b42371e3d535dc29a6f5a569aa53
alfimonth <alvy1610@gmail.com>
2023-06-13 21:52:44 +0700

    membuat halaman account

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -7,8 +7,102 @@ class AccountScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Akun'),
       ),
-      body: Center(
-        child: Text('Halaman Akun'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // const CircleAvatar(
+                //   radius: 40,
+                //   backgroundImage: NetworkImage('https://example.com/profile_image.jpg'), // Ganti dengan URL gambar profil
+                // ),
+                const Icon(
+                  Icons.account_circle, // Ganti dengan ikon profil yang sesuai
+                  size: 80,
+                  color: Colors.grey, // Ganti dengan warna ikon yang sesuai
+                ),
+
+                SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Nama Pengguna', // Ganti dengan nama pengguna
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'email@example.com', // Ganti dengan alamat email
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+                Spacer(),
+                IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    // Tambahkan logika untuk mengubah profil pengguna
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Riwayat Posting',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            Container(
+              height: 120, // Tentukan tinggi card
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 5, // Ganti dengan jumlah postingan yang sebenarnya
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 100, // Tentukan lebar gambar
+                          height: 80, // Tentukan tinggi gambar
+                          child: Image.network(
+                            'https://example.com/post_image.jpg',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'Caption Postingan',
+                            style: TextStyle(fontSize: 14),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 16),
+            ListTile(
+              leading: Icon(Icons.lock),
+              title: Text('Ubah Password'),
+              onTap: () {
+                // Tambahkan logika untuk menangani aksi ubah password
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Logout'),
+              onTap: () {
+                // Tambahkan logika untuk menangani aksi logout
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
